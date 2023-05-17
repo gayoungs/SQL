64f777b8030d6cbf16230639c9e5d9fad632a3c9
gayoungs <fairypetal0114@gmail.com>
2023-05-17 17:16:47 +0900

    sql commit

---
+++ 01SELECT절.sql
@@ -0,0 +1,35 @@
+-- 오라클 주석문
+/*
+여려줄 주석
+오라클은 대소문자 구분하지 않는다.
+*/
+SELECT * FROM EMPLOYEES;
+SELECT FIRST_NAME, EMAIL, HiRE_DATE FROM EMPLOYEES;
+SELECT JOB_ID, SALARY, DEPARTMENT_ID FROM EMPLOYEES;
+SELECT * FROM DEPARTMENTS;
+
+--연산
+--컬럼을 조회하는 위치에서 * / + -
+SELECT FIRST_NAME, SALARY, SALARY + SALARY * 0.1 FROM EMPLOYEES;
+
+--NULL
+SELECT FIRST_NAME, COMMISSION_PCT FROM EMPLOYEES;
+
+-- 엘리어서 as 별칭 아무 컬럼에 사용 가능
+SELECT FIRST_NAME AS 이름, 
+       LAST_NAME AS 성,
+       SALARY 급여, 
+       SALARY *0.1 총급여 
+FROM EMPLOYEES;
+
+--문자열의 연결 ||
+--오라클은 문자를 ''로 표현한다. 문자열 안에서 '를 사용하고 싶으면 두 번쓰면 된다.''
+SELECT FIRST_NAME || ' ' || LAST_NAME || '''is salary $' || SALARY AS 급여내역 
+FROM EMPLOYEES;
+
+--DISTINCT 중복행 제거
+SELECT DISTINCT FIRST_NAME, DEPARTMENT_ID FROM EMPLOYEES; -- 이름, 부서명이 동일한 경우 중복행 제거
+SELECT DISTINCT DEPARTMENT_ID FROM EMPLOYEES;
+
+--ROWID(데이터의 주소), ROWNUM(조회된 순서)
+SELECT ROWNUM, ROWID, EMPLOYEE_ID, FIRST_NAME FROM EMPLOYEES;
